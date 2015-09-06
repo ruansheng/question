dca7356ccc9e6a9cbec6fa54b39b4764306a7f5d
ruan.sheng <ruan.sheng@immomo.com>
2015-09-06 13:51:47 +0800

    add file

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 
 var app = express();
+var hbs = require('hbs');
 
 app.use(express.static('public'));
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
 
 app.get('/',index);
 
@@ -10,7 +13,8 @@ app.get('/',index);
 * index
 */
 function index(req, res) {
-	res.send('hello world !');
+	//res.send('hello world !');
+	res.render('index',{'title':'question'});
 }
 
 app.listen(3000);
