e98bf2f0f84fb0b5b88e30aa6ce7df3765b0ed08
ruan.sheng <ruan.sheng@immomo.com>
2015-09-07 22:43:54 +0800

    import css js

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var hbs = require('hbs');
 
 var routes = require('./routes/routes').routes;
-
+var logger = require('./lib/logger').logger;
 var login_controller = require('./controller/login_controller').login_controller;
 var index_controller = require('./controller/index_controller').index_controller;
 
 var app = express();
+
+app.set('port', process.ENV || 3000);
 
 app.use(express.static('public'));
 app.set('view engine', 'html');
@@ -24,12 +26,19 @@ app.use(function(req,res,next){
 
 /**
  * login routes
+ * http://localhost:port/login
  */
 app.get(routes.login, login_controller.login);
 
 /**
  * index routes
+ * http://localhost:port
  */
 app.get(routes.index, index_controller.index);
 
-app.listen(3000);
+/**
+ * listen
+ */
+app.listen(app.get('port'), function(){
+	logger.echo('question is running at localhost:' + app.get('port'));
+});
