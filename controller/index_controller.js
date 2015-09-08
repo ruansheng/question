be0adcbf10b5ff675bd9d90a250270b6c7e0d270
ruan.sheng <ruan.sheng@immomo.com>
2015-09-08 23:42:08 +0800

    db

--- controller/index_controller.js
+++ controller/index_controller.js
@@ -1,5 +1,8 @@
+var manager = require('../model/manager_model').manager;
+
 exports.index_controller = {
     "index" : function(req, res){
-        res.render('index',{'title':'question'});
+        manager.findManager('825307513@qq.com','123456');
+        //res.render('index',{'title':'question'});
     }
 }
---
+++ model/manager_model.js
@@ -0,0 +1,8 @@
+var logger = require('../lib/logger').logger;
+var db = require('../lib/mysql').db;
+
+exports.manager = {
+    "findManager": function(email,pass) {
+        db.find('825307513','123456');
+    }
+}
